2c7d6afe53cc259d1129f46d7c66863b4542ebfb
xursand <xursand@gmail.com>
2026-02-17 17:26:00 +0500

    inforest

---
+++ src/Components/Info.jsx
@@ -0,0 +1,27 @@
+import Rasm from "/bg.png";
+import Praz from "/prazrachniy.png";
+
+export default function Info() {
+  return (
+    <div>
+      <div className="container mx-auto relative">
+        <img className="w-full" src={Rasm} alt="Rasm" />
+        <img className="absolute top-0 w-full" src={Praz} alt="Prazrachniy" />
+        <div className="absolute top-[50%] left-[50%] text-center translate-x-[-50%] translate-y-[-50%] max-w-[746px] w-full">
+          <h2 className="text-white">Welcome to Local Face</h2>
+          <p className="text-white">
+            Welcome to Local Face Perfumes, where the spirit of victory and
+            triumph come alive through scents that empower and inspire. Our
+            curated collection, aptly named "Victory Scented," is a celebration
+            of success and elegance, designed to unleash your victorious
+            essence. Indulge in the sweet taste of triumph with captivating
+            fragrances that tell the tale of your achievements. At Local Face,
+            we believe that every victory deserves a signature scent, and we are
+            dedicated to providing unforgettable fragrances that elevate your
+            spirit and empower your journey.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,13 @@
-import Banner from '../../public/Banner.png'
+import React from 'react'
+import Banner from '../Components/Banner';
+import Info from '../Components/Info';
+
 
 export default function Home() {
   return (
-    <div className="text-white">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className=''>
-          <h1 className='text-6xl font-light max-w-[752px] w-full mb-[14px]'>Elevate Your Spirit with Victory Scented Fragrances!</h1>
-          <p className='text-[24px] font-light mb-[64px] max-w-[532px] w-full'> Shop now and embrace the sweet smell of victory with Local Face.</p>
-          <button className='bg-[#AB572D] py-[14px] px-[30px] rounded-xl'>Shop Now</button>
-        </div>
-
-        <img  src={Banner} alt="Banner" />
-      </div>
+    <div>
+      <Banner />
+      <Info/>
     </div>
-  );
+  )
 }
